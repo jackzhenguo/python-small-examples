058e884818661bc980740c1748d5f1e8c7c3e615
jack <guozhennianhua@163.com>
2019-11-24 11:03:54 +0800

    turtle3

--- src/test.py
+++ src/test.py
@@ -1,6 +1,14 @@
+class Book(object):
+    # 定义类的参数
+    def __init__(self, book_id, book_name, book_store_count):
+        self.book_id = book_id
+        self.book_name = book_name
+        self.book_store_count = book_store_count
+    # 定义加法操作
 
-def swap(a, b):
-    return b, a
+    def __add__(self, book):
+        return self.book_store_count + book.book_store_count
 
 
-print(swap(1, 0))  # (0,1)
+store_count = Book(1, 'python入门书', 100) + Book(2, '机器学习入门书', 200)
+print(store_count)
