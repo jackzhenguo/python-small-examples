7c9357dc00920e1929a50ba7a216627548e58a3c
guozhen3 <guozhen3@jd.com>
2019-11-26 13:47:30 +0800

    generator

--- src/divide_iter.py
+++ src/divide_iter.py
@@ -1,21 +1,16 @@
+from math import ceil
+
+
 def divide_iter(lst, n):
-    i, div = 0, len(lst) // n
+    if n <= 0:
+        yield lst
+        return
+    i, div = 0, ceil(len(lst) / n)
     while i < n:
         yield lst[i * div: (i + 1) * div]
         i += 1
 
 
+list(divide_iter([1, 2, 3, 4, 5], 0))  # [[1, 2, 3, 4, 5]]
 list(divide_iter([1, 2, 3, 4, 5], 2))  # [[1, 2, 3], [4, 5]]
 
-
-def divide(lst, size=2):
-    from itertools import islice
-    if size <= 0:
-        yield list(lst)
-    i, div = 0, len(lst) // size
-    while i < size:
-        yield list(islice(lst, i*div, (i + 1)*div))
-        i += 1
-
-
-print(list(divide([1, 2, 3, 4, 5], 2)))  # [[1, 2, 3], [4, 5]]
--- 将list等分为子组(生成器版).md
+++ 将list等分为子组(生成器版).md
@@ -1,5 +1,5 @@
 ---
-title: list分组的生成器实现方法
+title: list分组实现(生成器版本)
 tags: generator,list
 ---
 
@@ -8,11 +8,16 @@ from math import ceil
 
 
 def divide_iter(lst, n):
+    if n <= 0:
+        yield lst
+        return
     i, div = 0, ceil(len(lst) / n)
     while i < n:
         yield lst[i * div: (i + 1) * div]
         i += 1
 
 
+list(divide_iter([1, 2, 3, 4, 5], 0))  # [[1, 2, 3, 4, 5]]
 list(divide_iter([1, 2, 3, 4, 5], 2))  # [[1, 2, 3], [4, 5]]
+
 ```
