d7487d65c77bc86d283e6521583ddfbc05f330dc
jack <guozhennianhua@163.com>
2019-11-30 14:24:18 +0800

    read

--- src/turtlesnow.py
+++ src/turtlesnow.py
@@ -20,29 +20,11 @@ def snow(snow_count):
             p.backward(snowsize)  # 向当前画笔相反方向移动snowsize像素长度
             p.right(360 / dens)  # 顺时针移动360 / dens度
 
-def ground(ground_line_count):
-    p.hideturtle()
-    p.speed(500)
-    for i in range(ground_line_count):
-        p.pensize(random.randint(5, 10))
-        x = random.randint(-400, 350)
-        y = random.randint(-280, -1)
-        r = -y / 280
-        g = -y / 280
-        b = -y / 280
-        p.pencolor(r, g, b)
-        p.penup()  # 抬起画笔
-        p.goto(x, y)  # 让画笔移动到此位置
-        p.pendown()  # 放下画笔
-        p.forward(random.randint(40, 100))  # 眼当前画笔方向向前移动40~100距离
         
 def main():
     p.setup(800, 600, 0, 0)
-    # p.tracer(False)
     p.bgcolor("black")
     snow(30)
-    ground(30)
-    # p.tracer(True)
     p.mainloop()
 
 main()
