67dab6111b604e3d15e125f86aeb537940b31533
jack <guozhennianhua@163.com>
2019-12-08 11:32:34 +0800

    conf.py

--- rst/conf.py
+++ rst/conf.py
@@ -119,3 +119,19 @@ texinfo_documents = [
    u'zhenguo', 'python3-small-examples', '《Python 小例子》',
    'Miscellaneous'),
 ]
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+_exts = "../exts"
+sys.path.append(os.path.abspath(_exts))
+
