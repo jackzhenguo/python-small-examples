973fc597e1517f5d1b109067a437162b25cc9fa6
guozhen3 <guozhen3@jd.com>
2019-12-24 20:27:30 +0800

    del

--- src/top_big_files.py
+++
@@ -1,7 +0,0 @@
-from collections import defaultdict
-d = defaultdict(list)
-for root, dirs, files in os.walk('E:\guozhen3\08LOL\JDLOL\fengbi\jdlol'):
-    for file in files:
-        abs_path_file = os.path.join(root, file)
-        file_size = os.path.getsize(abs_path_file)
-        d[abs_path_file].append(file_size)
