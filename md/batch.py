760683eed7afd222cd6f1f76b9b33ab661542eec
jackzhenguo <guozhennianhua@163.com>
2020-08-23 12:40:05 +0800

    md-head

--- md/batch.py
+++ md/batch.py
@@ -1,6 +1,14 @@
 def batchCreate(ps='.md',start=100,n=100,path='.'):
     import os
+    import calendar
     for i in range(start,start+n):
         with open(path+'/'+str(i)+ps,'w') as fw:
-            fw.write('')
+            
+            fw.write("""
+                       ```markdown
+			@author jackzhenguo
+			@desc 
+			@date 2020/1/1
+			```
+		     """.format()
     print('done')
