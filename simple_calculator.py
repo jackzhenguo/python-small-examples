9be53f8cb2369acfd2b819aad480568a6569e317
Lakshya Dhariwal <lakshyadhariwal9@gmail.com>
2020-10-11 13:30:20 +0530

    Create simple_calculator.py
    I have created a simple calculator in python

---
+++ simple_calculator.py
@@ -0,0 +1,17 @@
+#Text based calculator in python
+
+no_a = int(input("enter first number:"))
+no_b = int(input("enter second number:")\
+user_input = input("Enter operation( add / sub / multiply / divide)")
+operation =  user_input.lower()
+
+if operation == "add";
+  print("a+b", no_a + no_b)
+elif operation == "sub";
+  print("a - b", no_a - no_b)
+elif operation == "multiply";
+  print("a*b", no_a * no_b)
+elif operation == "divide";
+  print("a/b", no_a / no_b)
+else ;
+  print("Invalid Operation")
